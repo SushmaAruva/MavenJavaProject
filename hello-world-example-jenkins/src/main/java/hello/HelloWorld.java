feda9230794d69ceaf5f13d655f88d49017cc9e7
Sushma Aruva <sushma.aruva@gmail.com>
2018-08-08 14:37:25 +0530

    Maven Jenkins Example Training

---
+++ hello-world-example-jenkins/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+// HelloWorld
+// by Naga
+public class HelloWorld {
+	public static void main(String[] args) {
+		Message message = new Message();
+		System.out.println(message.sayHello());
+	}
+}
+
+// comment - Nag
---
+++ hello-world-example-jenkins/src/main/java/hello/Message.java
@@ -0,0 +1,8 @@
+package hello;
+
+// by Nag
+public class Message {
+	public String sayHello() {
+		return "Hi,How are you?";
+	}
+}
